8c088a72143f4049da7f04951241f6d6443f756b
Lei Wang <raylwang@amazon.com>
2023-10-07 17:04:48 +0800

    update x264 benchmark

--- benchmark/264to264_benchmark_cpu.sh
+++ benchmark/264to264_benchmark_cpu.sh
@@ -2,23 +2,26 @@
 # Script to do benchmark test for CPU transcoding performance with h.264 codec settings,
 # log the performance result (frames per second) and upload the transcoded files to given s3 bucket
 #
-# Usage:    source 264to264_benchmark_cpu.sh {batch size} {ec2 instance type} {transcoding bitrate}
+# Usage:    bash 264to264_benchmark_cpu.sh <batch size> <transcoding bitrate> <s3 bucket name> <filename>
 #
-#           batch size: concurrent number of ffmpeg transcoding process (make cpu utilization close to 100%)
-#           ec2 instance type: instance type of ec2 for transcoding
-#           transcoding bitrate: bitrate for transcoding, e.g. 800k, 2M
+#           batch size:             concurrent number of ffmpeg transcoding process (make cpu utilization close to 100%)
+#           transcoding bitrate:    bitrate for transcoding, e.g. 800k, 2M
+#           s3 bucket name:         name of the bucket, which should include 'input' and 'output' folders
+#           file name:              name of the sample file
 #
-# Sample:   source 264to264_benchmark_cpu.sh 5 C6g.4x 2.5M
+# Sample:   bash 264to264_benchmark_cpu.sh 5 2.5M ffmpeg-on-graviton-ap-northeast-1 bbb_sunflower_1080p_30fps_normal.mp4
 #
 # Please make sure INPUT_BUCKET, OUTPUT_BUCKET and INPUT_FILE are set properly
 #
 
+sudo dnf -y install dmidecode
+
 BATCH_SIZE=$1
-INSTANCE=$2
-BITRATE=$3
-INPUT_BUCKET=s3://{input files bucket}/input/
-OUTPUT_BUCKET=s3://{output files bucket}/output/
-INPUT_FILE={input file}
+INSTANCE=$(sudo dmidecode -s system-product-name)
+BITRATE=$2
+INPUT_BUCKET=s3://$3/input/
+OUTPUT_BUCKET=s3://$3/output/
+INPUT_FILE=$4
 INPUT_FOLDER=`pwd`/input
 OUTPUT_FOLDER=`pwd`/output/${INSTANCE}
 LOGS_FOLDER=`pwd`/logs/${INSTANCE}
@@ -35,10 +38,6 @@ if [ -z $1 ]; then
 fi
 
 if [ -z $2 ]; then
-    INSTANCE="C5.4x"
-fi
-
-if [ -z $3 ]; then
     BITRATE=3M
 fi
 
@@ -54,9 +53,9 @@ batch_transcoding_process() {
         echo "batch job id $n start..."
         if [ $n -eq $((BATCH_SIZE-1)) ]; then
             echo "please waiting for the transcoding jobs complete..."
-            nohup bash -c "(time ffmpeg -y -hwaccel auto -i ${INPUT_FOLDER}/${INPUT_FILE} -c:v ${CODEC} -pix_fmt yuv420p -profile:v ${PROFILE} -preset faster -rc-lookahead:v 0 -bf:v 2 -coder:v cabac -b:v ${BITRATE} -c:a copy ${TEMP_FOLDER}/${output}_$n.mp4 2>&1 ) > ${temp_log}_$n.log 2>&1" >/dev/null 2>&1
+            ffmpeg -y -hwaccel auto -i ${INPUT_FOLDER}/${INPUT_FILE} -c:v ${CODEC} -pix_fmt yuv420p -profile:v ${PROFILE} -preset faster -rc-lookahead:v 0 -bf:v 2 -coder:v cabac -b:v ${BITRATE} -c:a copy ${TEMP_FOLDER}/${output}_$n.mp4 2>&1 | tee ${temp_log}_$n.log 
         else
-            nohup bash -c "(time ffmpeg -y -hwaccel auto -i ${INPUT_FOLDER}/${INPUT_FILE} -c:v ${CODEC} -pix_fmt yuv420p -profile:v ${PROFILE} -preset faster -rc-lookahead:v 0 -bf:v 2 -coder:v cabac -b:v ${BITRATE} -c:a copy ${TEMP_FOLDER}/${output}_$n.mp4 2>&1 ) > ${temp_log}_$n.log 2>&1 &" >/dev/null 2>&1
+            ffmpeg -y -hwaccel auto -i ${INPUT_FOLDER}/${INPUT_FILE} -c:v ${CODEC} -pix_fmt yuv420p -profile:v ${PROFILE} -preset faster -rc-lookahead:v 0 -bf:v 2 -coder:v cabac -b:v ${BITRATE} -c:a copy ${TEMP_FOLDER}/${output}_$n.mp4 2>&1 | tee ${temp_log}_$n.log
         fi
     done
 
@@ -69,17 +68,21 @@ batch_transcoding_process() {
 
     for ((n=0;n<$BATCH_SIZE;n++)); do
 
+        # Get CPU usage
+        #user=`cat ${temp_log}_$n.log | awk '/user/ {print $2}' | awk -F'm' '{printf 60*int($1*100)/100+int($2*100)/100}'`
+        #real=`cat ${temp_log}_$n.log | awk '/real/ {print $2}' | awk -F'm' '{printf 60*int($1*100)/100+int($2*100)/100}'`
+        #cpu_usage=`echo "scale=4;$user/$real/$VCPU*100;" | bc`
+        #cpu_usage_total=`echo "$cpu_usage_total+$cpu_usage" | bc`
+
         # Get ffmpeg transcoding frames per second 
         fps=`cat ${temp_log}_$n.log | awk -F'=' '/fps=/ {print $(NF-7)}' | awk '{print $1}'`
         fps_total=`echo "$fps_total+$fps" | bc`
-        # Get CPU usage
-        user=`cat ${temp_log}_$n.log | awk '/user/ {print $2}' | awk -F'm' '{printf 60*int($1*100)/100+int($2*100)/100}'`
-        real=`cat ${temp_log}_$n.log | awk '/real/ {print $2}' | awk -F'm' '{printf 60*int($1*100)/100+int($2*100)/100}'`
-        cpu_usage=`echo "scale=4;$user/$real/$VCPU*100;" | bc`
-        cpu_usage_total=`echo "$cpu_usage_total+$cpu_usage" | bc`
+
         # Log cpu_usage and fps
-        echo "${output} trancode job #$n cpu_usage:$cpu_usage fps:${fps} frames per second"
-        echo "${output} trancode job #$n cpu_usage:$cpu_usage fps:${fps} frames per second" >> ${LOG_FILE}
+        #echo "${output} trancode job #$n cpu_usage:$cpu_usage fps:${fps} frames per second"
+        #echo "${output} trancode job #$n cpu_usage:$cpu_usage fps:${fps} frames per second" >> ${LOG_FILE}
+        echo "${output} trancode job #$n fps:${fps} frames per second"
+        echo "${output} trancode job #$n fps:${fps} frames per second" >> ${LOG_FILE}
 
     done
 
@@ -116,8 +119,8 @@ echo "======================================================" >> ${LOG_FILE}
 batch_transcoding_process 
 
 ## step 3 upload results
-cp ${TEMP_FOLDER}/${output}_0.mp4 ${OUTPUT_FOLDER}/"${INPUT_FILE%.*}_${CODEC}_${BITRATE}.mp4"
-aws s3 sync ${OUTPUT_FOLDER} ${OUTPUT_BUCKET}${INSTANCE}
+#cp ${TEMP_FOLDER}/${output}_0.mp4 ${OUTPUT_FOLDER}/"${INPUT_FILE%.*}_${CODEC}_${BITRATE}.mp4"
+#aws s3 sync ${OUTPUT_FOLDER} ${OUTPUT_BUCKET}${INSTANCE}
 
 ## step 4 clean jobs
-rm -rf ${TEMP_FOLDER}
+#rm -rf ${TEMP_FOLDER}
